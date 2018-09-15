ef109f19f36a7d7473a5761de1d02e9a781332c5
Pham Minh Phuong <phuongninhbinh7696@gmail.com>
2018-09-15 20:59:00 +0700

    hihi

--- TimViecLam/Properties/Resources.Designer.cs
+++ TimViecLam/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace TimViecLam.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap upload_icon {
+            get {
+                object obj = ResourceManager.GetObject("upload-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
